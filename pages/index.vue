cfe6e9a5c76094ff748c50ee2527e1ad08020f0c
nagata <nagata@infosign.co.jp>
2019-10-24 18:26:58 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="items">
+    <!-- itemsをforでまわし、itemにそれぞれのデータが入ります。 -->
+    <div v-for="item in items" :key="item" class="item-box">
+      <div class="fresh"><a :href="item.url">{{ item.title }}</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// axiosの使用宣言
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      // 取得したデータを入れる
+      items: ''
+    }
+  },
+  // API通信
+  async asyncData () {
+    const { data } = await axios.get('https://infosign.microcms.io/api/v1/fresh', {
+      headers: { 'X-API-KEY': '20d4758a-5a3d-48ec-a014-d2e9990e004e' }
+    })
+    return {
+      items: data.contents
+    }
+  }
+}
+</script>
+
+<style>
+</style>
