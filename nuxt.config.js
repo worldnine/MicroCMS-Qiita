88787fe8eecafd7848fa8cf9ce68c6eaa5fbf3ce
worldnine <nagata@infosign.co.jp>
2019-10-24 20:27:48 +0900

    https://ja.nuxtjs.org/faq/github-pages/ の内容を反映

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
 
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/MicroCMS-Qiita/'
+  }
+} : {}
+
+
 export default {
   mode: 'spa',
   /*
@@ -57,5 +65,8 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  routor:{
+    base:{/MicroCMS-Qiita/}
   }
 }
